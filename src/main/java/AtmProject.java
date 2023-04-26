b34e972514646485db3f3e115eb767e85d5c462b
vehbitr <vehbidemirezen348@gmail.com>
2023-04-26 22:55:40 +0300

    vhb1

--- src/main/java/AtmProject.java
+++ src/main/java/AtmProject.java
@@ -8,6 +8,9 @@ public class AtmProject {
         //version3
         System.out.println("Git ogrenmek cok kolay;)");
         //version4
+
+        System.out.println("vehbi branch i kodlarini yazdi");
+        //version vhb1
     }
 
 }
