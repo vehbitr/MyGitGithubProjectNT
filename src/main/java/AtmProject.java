852477455e9bd52ac459541c65328e200be7b9ef
vehbitr <vehbidemirezen348@gmail.com>
2023-04-26 22:03:02 +0300

    v4

--- src/main/java/AtmProject.java
+++ src/main/java/AtmProject.java
@@ -6,6 +6,8 @@ public class AtmProject {
         //version2
         System.out.println("Learn Java Earn Money");
         //version3
+        System.out.println("Git ogrenmek cok kolay;)");
+        //version4
     }
 
 }
